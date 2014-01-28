25989cff0b82bfdbdabdca45418e9de679ab28be
Robert Vollmert <rvollmert@gmx.net>
2014-01-28 19:04:13 +0100

    a real thermo-sudoku example, from http://wiki.logic-masters.de/index.php?title=Thermo-Sudoku/en

--- examples/thermo.hs
+++ examples/thermo.hs
@@ -8,11 +8,20 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
 thermos = map (thermo . map p2)
-          [ [(1, 0), (1, 1), (2, 1), (2, 2)]
-          , [(3, 3), (4, 4), (5, 3)]
-          , [(6, 0), (6, 1), (7, 2), (7, 3), (8, 4), (7, 5), (6, 5)]
+          [ [(0, 4), (0, 8)]
+          , [(0, 3), (0, 0), (2, 0)]
+          , [(3, 0), (8, 0), (8, 2)]
+          , [(8, 5), (8, 3)]
+          , [(8, 8), (8, 6)]
+          , [(5, 8), (7, 8)]
+          , [(1, 8), (4, 8)]
+          , [(2, 2), (6, 2)]
+          , [(2, 4), (6, 4), (6, 3)]
+          , [(2, 6), (6, 6), (6, 5)]
           ]
 
-d = (sudokugrid `atop` mconcat thermos) # frame
+clues = [ ((1, 1), 1), ((1, 7), 3), ((7, 1), 2), ((7, 7), 7) ]
+
+d = (drawClues clues `atop` sudokugrid `atop` mconcat thermos) # frame
 
 main = defaultMain d
