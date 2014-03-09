543912e66573f519ab0f8827803e39f3e609d990
Robert Vollmert <rvollmert@gmx.net>
2014-03-09 09:52:34 +0100

    Simplify RenderPuzzle type.
    Both puzzle and solution renderer should always be
    applied to the same argument, so better make the
    type imply that.

--- src/Diagrams/TwoD/Puzzles/Puzzle.hs
+++ src/Diagrams/TwoD/Puzzles/Puzzle.hs
@@ -15,13 +15,12 @@ import Data.Puzzles.ReadPuzzle hiding (puzzle, solution)
 import Data.Puzzles.Grid
 import Data.Puzzles.Pyramid
 
-data RenderPuzzle b a = RenderPuzzle
-                        { puzzle   :: a -> Diagram b R2
-                        , solution :: a -> Diagram b R2
-                        }
+import Control.Applicative (liftA2)
+
+type RenderPuzzle b a = a -> (Diagram b R2, Diagram b R2)
 
 lits :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b LITS
-lits = RenderPuzzle
+lits = liftA2 (,)
        (drawAreaGridG . pzl)
        (drawAreaGrid . pzl <> drawShadedGrid . sol)
 
@@ -29,58 +28,58 @@ solstyle :: HasStyle a => a -> a
 solstyle = lc (blend 0.8 black white)
 
 geradeweg :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Geradeweg
-geradeweg = RenderPuzzle
+geradeweg = liftA2 (,)
      (drawIntGrid . pzl)
      (drawIntClues . pzl
       <> solstyle . drawDualEdges . sol
       <> drawGrid . pzl)
 
 fillomino :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Fillomino
-fillomino = RenderPuzzle (drawFillo . pzl) (drawFillo . sol)
+fillomino = liftA2 (,) (drawFillo . pzl) (drawFillo . sol)
 
 masyu :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Masyu
-masyu = RenderPuzzle (drawMasyuGrid . pzl)
-                     (solstyle . drawDualEdges . sol <> drawMasyuGrid . pzl)
+masyu = liftA2 (,) (drawMasyuGrid . pzl)
+                   (solstyle . drawDualEdges . sol <> drawMasyuGrid . pzl)
 
 nurikabe :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Nurikabe
-nurikabe = RenderPuzzle (drawIntGrid . pzl)
-                        (drawIntGrid . pzl <> drawShadedGrid . sol)
+nurikabe = liftA2 (,) (drawIntGrid . pzl)
+                      (drawIntGrid . pzl <> drawShadedGrid . sol)
 
 latintapa :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b LatinTapa
-latintapa = RenderPuzzle
+latintapa = liftA2 (,)
             l
             (l <> atCentres drawChar . clues . sol)
     where l = (drawGrid <> drawWordsClues) . pzl
 
 sudoku :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Sudoku
-sudoku = RenderPuzzle
+sudoku = liftA2 (,)
          ((drawIntClues <> sudokugrid) . pzl)
          ((drawIntClues <> sudokugrid) . sol)
 
 thermosudoku :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b ThermoSudoku
-thermosudoku = RenderPuzzle
+thermosudoku = liftA2 (,)
                ((drawIntClues . fst <> sudokugrid . fst <> drawThermos . snd) . pzl)
                (drawIntClues . sol <> sudokugrid . sol <> drawThermos . snd . pzl)
 
 pyramid :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b PPyramid
-pyramid = RenderPuzzle
+pyramid = liftA2 (,)
           (Pyr.pyramid . pzl)
           (Pyr.pyramid . merge)
     where merge (PP p q) = mergepyramids p q
 
 kpyramid :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b PKropkiPyramid
-kpyramid = RenderPuzzle
+kpyramid = liftA2 (,)
           (Pyr.kpyramid . pzl)
           (Pyr.kpyramid . merge)
     where merge (PP p q) = mergekpyramids p q
 
 slither :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b SlitherLink
-slither = RenderPuzzle
+slither = liftA2 (,)
           (drawSlitherGrid . pzl)
           (drawSlitherGrid . pzl <> solstyle . drawedges . sol)
 
 liarslither :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b LiarSlitherLink
-liarslither = RenderPuzzle
+liarslither = liftA2 (,)
               (drawSlitherGrid . pzl)
               (solstyle . drawCrosses . snd . sol
                <> drawSlitherGrid . pzl
@@ -88,7 +87,7 @@ liarslither = RenderPuzzle
 
 tightfitskyscrapers :: (Backend b R2, Renderable (Path R2) b) =>
                        RenderPuzzle b TightfitSkyscrapers
-tightfitskyscrapers = RenderPuzzle
+tightfitskyscrapers = liftA2 (,)
     (atCentres drawInt . clueso . fst . pzl
      <> drawTightGrid (const mempty) . snd . pzl)
     (atCentres drawInt . clueso . fst . pzl
@@ -99,41 +98,44 @@ wordgrid :: (Backend b R2, Renderable (Path R2) b) =>
 wordgrid g ws = stackWords ws `besidesR` drawClueGrid g
 
 wordloop :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Wordloop
-wordloop = RenderPuzzle (uncurry wordgrid . pzl) (drawClueGrid . sol)
+wordloop = liftA2 (,) (uncurry wordgrid . pzl) (drawClueGrid . sol)
 
 wordsearch :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Wordsearch
-wordsearch = RenderPuzzle
+wordsearch = liftA2 (,)
              (uncurry wordgrid . pzl) 
              (solstyle . drawMarkedWords . snd . sol
               <> drawClueGrid . fst . sol)
 
 curvedata :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b CurveData
-curvedata = RenderPuzzle
+curvedata = liftA2 (,)
             cd
             ((solstyle . drawDualEdges . sol) <> cd)
      where cd = (atCentres drawCurve . clues <> drawGrid) . pzl
 
 doubleback :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b DoubleBack
-doubleback = RenderPuzzle
+doubleback = liftA2 (,)
              (drawAreaGridG . pzl)
              (solstyle . drawDualEdges . sol <> drawAreaGridG . pzl)
 
 slalom :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Slalom
-slalom = RenderPuzzle
+slalom = liftA2 (,)
          (drawSlalomGrid . pzl)
          (drawSlalomGrid . pzl <> solstyle . drawSlalomDiags . sol)
 
 compass :: (Backend b R2, Renderable (Path R2) b) => RenderPuzzle b Compass
-compass = RenderPuzzle
+compass = liftA2 (,)
           (drawCompassGrid . pzl)
           (drawCompassClues . pzl <> drawAreaGridG . sol)
 
 data OutputChoice = DrawPuzzle | DrawSolution | DrawExample
 
+-- | Combine a puzzle renderer with its input to optionally
+--   render the puzzle, its solution, or a side-by-side
+--   example with puzzle and solution.
 draw :: (Backend b R2, Renderable (Path R2) b) =>
         RenderPuzzle b a -> a -> OutputChoice -> Diagram b R2
-draw rp p DrawPuzzle   = puzzle rp p # bg white
-draw rp p DrawSolution = solution rp p # bg white
-draw rp p DrawExample  = (puzzle rp p
-                          ||| strutX 2.0
-                          ||| solution rp p) # bg white
+draw rp x o = d o # bg white
+    where (p, s) = rp x
+          d DrawPuzzle   = p
+          d DrawSolution = s
+          d DrawExample  = p ||| strutX 2.0 ||| s
