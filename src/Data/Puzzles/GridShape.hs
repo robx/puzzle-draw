028fdfee2af5d11f5919dd886f0b9d276ae72181
Robert Vollmert <rvollmert@gmx.net>
2014-03-13 22:24:05 +0100

    Towards more generic grid shapes.
    Also looking at fixing the bugs with gaps in grid data.

---
+++ src/Data/Puzzles/GridShape.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Data.Puzzles.GridShape where
+
+class GridShape a where
+    type GridSize a :: *
+    type Cell     a :: *
+    type Vertex   a :: *
+
+    size :: a -> GridSize a
+    cells :: a -> [Cell a]
+    vertices :: a -> [Vertex a]
+
+data Square = Square Int Int
+
+instance GridShape Square where
+    type GridSize Square = (Int, Int)
+    type Cell Square     = (Int, Int)
+    type Vertex Square   = (Int, Int)
+
+    size (Square w h)     = (w, h)
+    cells (Square w h)    = [(x, y) | x <- [0..w-1], y <- [0..h-1]]
+    vertices (Square w h) = [(x, y) | x <- [0..w], y <- [0..h]]
+
+--data Dir = V | H
+--type EdgeI = (VertexI, Dir)
+--size (SquareGrid w h) = (w, h)
+
+--cellPos :: SquareGrid -> CellI -> P2
+--vertexPos :: SquareGrid -> VertexI -> P2
+
+--Ord CellI, VertexI?
+
+--Edge = (CellI, CellI)? (VertexI, VertexI)?
+--DualEdge = (CellI, CellI)?
