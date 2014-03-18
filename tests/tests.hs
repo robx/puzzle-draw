b37d9a6702db9d99bca383580c454715a0b0851e
Robert Vollmert <rvollmert@gmx.net>
2014-03-18 14:27:13 +0100

    More tests that test more.
    Tests weren't failing earlier, since the Yaml parsing succeeded,
    and the errors were hidden due to missing evaluation. Hack around
    this by Show'ing the parsed value.

--- tests/tests.hs
+++ tests/tests.hs
@@ -3,7 +3,6 @@ import Test.Tasty.HUnit
 
 import Data.Text (Text, pack)
 import Data.Yaml
-import Data.Maybe (isJust)
 
 import Data.Puzzles.ReadPuzzle (geradeweg', tightfitskyscrapers')
 
@@ -44,11 +43,34 @@ tightfit_1_sol = pack . unlines $
     , "   3  1\\2 5/4"
     ]
 
-testParse p t = isJust (parseMaybe p (String t)) @? "parsing failed"
+tightfit_sol_broken :: Text
+tightfit_sol_broken = pack "2/1 4 /5"
+
+tightfit_sol_broken_2 :: Text
+tightfit_sol_broken_2 = pack "2/x 4 3/5"
+
+justShowLength :: Show a => Maybe a -> Int
+justShowLength = maybe (-1) (length . show)
+
+checkParse :: Show a => Int -> (Value -> Parser a) -> Text -> (Bool, String)
+checkParse n p t = (l >= n, "bad parse, show length " ++ show l ++ " < " ++ show n)
+  where
+    l = justShowLength (parseMaybe p (String t))
+testParse n p t = b @? e
+  where
+    (b, e) = checkParse n p t
+
+testNonparse p t = l == (-1) @? "parsed but shouldn't, length " ++ show l
+  where
+    l = justShowLength (parseMaybe p (String t))
 
 unitTests = testGroup "Unit tests"
-    [ testCase "parse geradeweg" $ testParse (fst geradeweg') geradeweg_1
-    , testCase "parse geradeweg solution" $ testParse (snd geradeweg') geradeweg_1_sol
-    , testCase "parse tightfit" $ testParse (fst tightfitskyscrapers') tightfit_1
-    , testCase "parse tightfit solution" $ testParse (snd tightfitskyscrapers') tightfit_1_sol
+    [ testCase "parse geradeweg" $ testParse 442 (fst geradeweg') geradeweg_1
+    , testCase "parse geradeweg solution" $ testParse 221 (snd geradeweg') geradeweg_1_sol
+    , testCase "parse tightfit" $ testParse 263 (fst tightfitskyscrapers') tightfit_1
+    , testCase "parse tightfit solution" $ testParse 169 (snd tightfitskyscrapers') tightfit_1_sol
+    , testCase "don't parse broken tightfit solution" $
+        testNonparse (snd tightfitskyscrapers') tightfit_sol_broken
+    , testCase "don't parse broken tightfit solution" $
+        testNonparse (snd tightfitskyscrapers') tightfit_sol_broken_2
     ]
