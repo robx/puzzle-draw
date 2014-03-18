a87359fe72dce1c5e7da91a543c9aac6c4101384
Robert Vollmert <rvollmert@gmx.net>
2014-03-18 19:23:36 +0100

    more annotations

--- src/Diagrams/TwoD/Puzzles/Things.hs
+++ src/Diagrams/TwoD/Puzzles/Things.hs
@@ -12,6 +12,8 @@ import Diagrams.TwoD.Puzzles.Lib
 import Diagrams.TwoD.Puzzles.Widths
 import Diagrams.TwoD.Puzzles.Grid
 
+pearl :: (Renderable (Path R2) b, Backend b R2) =>
+         MasyuPearl -> Diagram b R2
 pearl MWhite = circle 0.35 # lw 0.05
 pearl MBlack = pearl MWhite # fc black
 
@@ -27,6 +29,8 @@ cross = ur <> dr
 drawCross :: Renderable (Path R2) b => Diagram b R2
 drawCross = stroke cross # scale 0.8 # lw edgewidth
 
+drawCompassClue :: (Renderable (Path R2) b, Backend b R2) =>
+                   CompassC -> Diagram b R2
 drawCompassClue (CC n e s w) = texts <> stroke cross # lw onepix
     where tx Nothing _ = mempty
           tx (Just x) v = text' (show x) # scale 0.5 # translate (r2 v)
@@ -44,6 +48,8 @@ thermo vs@(v:_) = (bulb `atop` line) # col # translate (r2 (0.5, 0.5))
 drawThermos :: Renderable (Path R2) b => [Thermometer] -> QDiagram b R2 Any
 drawThermos = mconcat . map (thermo . map p2i)
 
+drawTight :: (Renderable (Path R2) b, Backend b R2) =>
+             (a -> Diagram b R2) -> Tightfit a -> Diagram b R2
 drawTight d (Single x) = d x
 drawTight d (UR x y) = stroke ur # lw onepix
                        <> d x # scale s # translate (r2 (-t,t))
@@ -66,16 +72,25 @@ drawMarkedWord (MW s e) = lw onepix . stroke $ expandTrail' with {_expandCap = L
 drawMarkedWords :: (Renderable (Path R2) b) => [MarkedWord] -> QDiagram b R2 Any
 drawMarkedWords = mconcat . map drawMarkedWord
 
+drawSlalomClue :: (Show a, Renderable (Path R2) b, Backend b R2) =>
+                  a -> Diagram b R2
 drawSlalomClue x = text' (show x) # scale 0.75
                    <> circle 0.4 # fc white # lw onepix
 
 drawText :: (Backend b R2, Renderable (Path R2) b) => String -> QDiagram b R2 Any
 drawText = text'
 
+drawInt :: (Renderable (Path R2) b, Backend b R2) =>
+           Int -> Diagram b R2
 drawInt s = drawText (show s)
+
+drawChar :: (Renderable (Path R2) b, Backend b R2) =>
+            Char -> Diagram b R2
 drawChar c = drawText [c]
 
 -- | Stack a list of words into a unit square.
+drawWords :: (Renderable (Path R2) b, Backend b R2) =>
+             [String] -> Diagram b R2
 drawWords ws = spread (-1.0 *^ unitY)
                       (map (centerXY . scale 0.4 . drawText) ws)
                # centerY
