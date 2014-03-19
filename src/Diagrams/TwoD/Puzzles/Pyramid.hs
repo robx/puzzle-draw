ad93db0c0d5b3c4fda1292ca88b9dfc41efa9892
Robert Vollmert <rvollmert@gmx.net>
2014-03-19 17:01:10 +0100

    silence some more warnings

--- src/Diagrams/TwoD/Puzzles/Pyramid.hs
+++ src/Diagrams/TwoD/Puzzles/Pyramid.hs
@@ -1,38 +1,48 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Diagrams.TwoD.Puzzles.Pyramid where
 
 import Diagrams.Prelude
-import Diagrams.Util
-import Diagrams.Combinators
 
 import Data.Puzzles.Pyramid
 import Diagrams.TwoD.Puzzles.Lib
 import Diagrams.TwoD.Puzzles.Widths
 import Diagrams.TwoD.Puzzles.Grid (frame)
 
+pgray :: Colour Double
 pgray = blend 0.6 white black
 
+cell :: (Backend b R2, Renderable (Path R2) b) => Bool -> Diagram b R2
 cell s = square 1 # lw onepix # if s then fc pgray else id
 
+clue :: (Backend b R2, Renderable (Path R2) b) => Maybe Int -> Diagram b R2
 clue Nothing = mempty
 clue (Just c) = text' (show c)
+
+cellc :: (Backend b R2, Renderable (Path R2) b) => Bool -> Maybe Int -> Diagram b R2
 cellc s c = clue c `atop` cell s
 
+row :: (Backend b R2, Renderable (Path R2) b) => Row -> Diagram b R2
 row (R cs s) = centerX . hcat . map (cellc s) $ cs
 
+pyramid :: (Backend b R2, Renderable (Path R2) b) => Pyramid -> Diagram b R2
 pyramid p = phantom (frame (s, s)) <> (alignBL . vcat . map row . unPyr $ p)
     where s = psize p
 
+kropki :: (Backend b R2, Renderable (Path R2) b) => KropkiDot -> Diagram b R2
 kropki None = mempty
 kropki c = circle 0.1 # lw 0.03 # fc (col c) # smash
     where col White = white
           col Black = blend 0.98 black white
+          col None  = error "can't reach"
 
-krow (KR cs s ks) = ccat dots `atop` ccat clues
+krow :: (Backend b R2, Renderable (Path R2) b) => KropkiRow -> Diagram b R2
+krow (KR cs s ks) = ccat dots <> ccat clues
     where ccat = centerX . hcat
           clues = map (cellc s) cs
-          dots = interleave (map phantom (clues :: [D R2])) (map kropki ks)
+          clues' = map (cellc s) cs :: [D R2]
+          dots = interleave (map phantom clues') (map kropki ks)
 
-kpyramid p = phantom (frame (s, s)) <> (alignBL . vcat . map krow . unKP $ p)
+kpyramid :: (Backend b R2, Renderable (Path R2) b) => RowKropkiPyramid -> Diagram b R2
+kpyramid p = phantom (frame (s, s) :: D R2) <> (alignBL . vcat . map krow . unKP $ p)
     where s = psize (plainpyramid p)
