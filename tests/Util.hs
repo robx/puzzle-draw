f4e168167fbd8e4c59a63754193c92967b60685d
Robert Vollmert <rvollmert@gmx.net>
2014-04-07 21:36:57 +0200

    Some test documentation.

--- tests/Util.hs
+++ tests/Util.hs
@@ -5,20 +5,25 @@ import Data.Yaml
 
 import Test.Tasty.HUnit
 
+-- | Force full evaluation of a showable value.
 justShow :: Show a => Maybe a -> Bool
 justShow Nothing = False
 justShow (Just x) = show x `deepseq` True
 
+-- | Force full evaluation of a showable value.
 eitherShow :: Show a => Either e a -> Bool
 eitherShow (Left _) = False
 eitherShow (Right x) = show x `deepseq` True
 
+-- | Test that a value is parsed correctly, by forcing the
+--   parse result to be fully evaluated.
 testParse :: Show a => (Value -> Parser a) -> Value -> Assertion
 testParse p t = eitherShow res @? "bad parse: " ++ err
   where
     res = parseEither p t
     err = either id (const "no error") res
 
+-- | Test that a value is not parsed.
 testNonparse :: Show a => (Value -> Parser a) -> Value -> Assertion
 testNonparse p t = (not . justShow . parseMaybe p $ t)
                    @? "parsed but shouldn't"
