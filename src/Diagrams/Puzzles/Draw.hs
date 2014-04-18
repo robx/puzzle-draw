1a8652c2cdf568248b2dca3d3311b10b20081a26
Robert Vollmert <rvollmert@gmx.net>
2014-04-18 13:03:02 +0200

    add sizing utilities from cmdline tool

--- src/Diagrams/Puzzles/Draw.hs
+++ src/Diagrams/Puzzles/Draw.hs
@@ -4,10 +4,14 @@ module Diagrams.Puzzles.Draw (
     PuzzleSol,
     RenderPuzzle,
     OutputChoice(..),
-    draw
+    draw,
+    Unit(..),
+    diagramWidth,
+    toOutputWidth,
   ) where
 
 import Diagrams.Prelude
+import Diagrams.BoundingBox
 
 type RenderPuzzle b p s = (p -> Diagram b R2, (p, s) -> Diagram b R2)
 
@@ -24,3 +28,18 @@ draw (p, ms) = fmap (bg white) . d
     where d DrawPuzzle   = Just p
           d DrawSolution = ms
           d DrawExample  = ms >>= \s -> return $ p ||| strutX 2.0 ||| s
+
+data Unit = Pixels | Points
+
+cmtopoint :: Double -> Double
+cmtopoint = (* 28.3464567)
+
+diagramWidth :: Diagram b R2 -> Double
+diagramWidth = fst . unr2 . boxExtents . boundingBox
+
+toOutputWidth :: Unit -> Double -> Double
+toOutputWidth u w = case u of Pixels -> fromIntegral wpix
+                              Points -> wpt
+  where
+    wpix = round (40 * w) :: Int  -- grid square size 40px
+    wpt = cmtopoint (0.8 * w)     -- grid square size 0.8cm
