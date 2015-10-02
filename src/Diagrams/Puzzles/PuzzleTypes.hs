bd0c1a631973e56304ca54c4cf8ff6fa626f0343
Robert Vollmert <rfvollmert@gmail.com>
2015-10-02 13:16:40 +0200

    fix another warning

--- src/Diagrams/Puzzles/PuzzleTypes.hs
+++ src/Diagrams/Puzzles/PuzzleTypes.hs
@@ -20,7 +20,7 @@ module Diagrams.Puzzles.PuzzleTypes (
     starbattle
   ) where
 
-import Diagrams.Prelude hiding (Loop, coral, size, N)
+import Diagrams.Prelude hiding (Loop, N, coral, inside, size)
 
 import qualified Data.Map as Map
 
