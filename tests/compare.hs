e5d31d283c2f39807fed7893f2fd402bb9186e50
Robert Vollmert <rob@vllmrt.net>
2016-10-04 12:08:54 +0200

    golden tests via makefile (fixes hard-coded executable path)

--- tests/compare.hs
+++ tests/compare.hs
@@ -11,11 +11,8 @@ import System.FilePath
 main :: IO ()
 main = tests >>= defaultMain
 
-drawpuzzle :: FilePath
-drawpuzzle = ".stack-work/install/x86_64-osx/lts-5.2/7.10.3/bin/drawpuzzle"
-
 render :: FilePath -> IO ()
-render fp = callProcess drawpuzzle [ "-e", "-c", "-f",  "png", fp ]
+render fp = callProcess "stack" ["exec", "drawpuzzle", "--", "-e", "-c", "-f",  "png", fp ]
 
 testFile :: FilePath -> TestTree
 testFile fp = goldenVsFile ("comparing " ++ fp)
@@ -26,9 +23,5 @@ testFile fp = goldenVsFile ("comparing " ++ fp)
     fppng = replaceExtension fp ".png"
 
 tests :: IO TestTree
-#ifdef CAIRO
 tests = testGroup "compare to old output" . map testFile
         <$> findByExtension [".pzl"] "tests/examples"
-#else
-tests = return $ testGroup "comparison disabled, requires cairo" []
-#endif
