a86fe289d700aeacc828eb3420b2c97724f6a31b
Robert Vollmert <rob@vllmrt.net>
2018-03-06 16:23:27 +0100

    rasterific backend

--- src/Draw/CmdLine.hs
+++ src/Draw/CmdLine.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, CPP #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Draw.CmdLine 
     ( B
@@ -14,31 +14,15 @@ import Data.CmdLine (exitErr)
 import Diagrams.Prelude hiding (value, option, (<>), Result)
 import Control.Monad (unless)
 
-#ifdef CAIRO
-import Diagrams.Backend.Cairo (B, renderCairo)
-#else
-import Diagrams.Backend.SVG (B, renderSVG)
-#endif
+import Diagrams.Backend.Rasterific (B, renderRasterific)
 
 data RenderOpts = RenderOpts { _file :: FilePath, _w :: Double }
 
-renderB :: FilePath -> SizeSpec V2 Double -> Diagram B -> IO ()
-renderB =
-#ifdef CAIRO
-    renderCairo
-#else
-    renderSVG
-#endif
-
 renderToFile :: RenderOpts -> Diagram B -> IO ()
-renderToFile ropts = renderB (_file ropts) (mkWidth $ _w ropts)
+renderToFile ropts = renderRasterific (_file ropts) (mkWidth $ _w ropts)
 
 formats :: [String]
-#ifdef CAIRO
-formats = ["png", "svg", "ps", "pdf"]
-#else
-formats = ["svg"]
-#endif
+formats = ["png", "ps", "pdf"]
 
 checkFormat :: String -> IO ()
 checkFormat f = unless (f `elem` formats) $
