11c0a6c1c4730af254a6adf5745f8ee3bf217bc4
Robert Vollmert <rob@vllmrt.net>
2018-07-31 16:36:45 +0200

    document framewidthfactor, kinda

--- src/Draw/Widths.hs
+++ src/Draw/Widths.hs
@@ -16,6 +16,8 @@ fourpix = 4 * onepix
 linewidth :: Double
 linewidth = onepix
 
+-- width of frame relative to width of thick edge
+-- for standard 3 pixel edgewidth this yields a 4 pixel frame
 framewidthfactor :: Double
 framewidthfactor = 4/3
 
