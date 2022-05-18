a45babb806f7a8ab1d0fb85fb36fa1c8b25db1d5
Robert Vollmert <rob@vllmrt.net>
2022-05-18 20:14:11 +0200

    cut down default.nix

--- web/default.nix
+++ web/default.nix
@@ -44,52 +44,22 @@ with (import nixpkgs config); let
       version = "1.0.0";
     };
   };
-  mkDerivation = {
-    src,
-    name,
-    srcdir ? "./src",
-    targets ? [],
-    registryDat ? ./registry.dat,
-    outputJavaScript ? false,
-  }:
-    stdenv.mkDerivation {
-      inherit name src;
-
-      buildInputs =
-        [elmPackages.elm]
-        ++ lib.optional outputJavaScript nodePackages.uglify-js;
-
-      buildPhase = pkgs.elmPackages.fetchElmDeps {
-        elmPackages = srcs;
-        elmVersion = "0.19.1";
-        inherit registryDat;
-      };
-
-      installPhase = let
-        elmfile = module: "${srcdir}/${builtins.replaceStrings ["."] ["/"] module}.elm";
-        extension =
-          if outputJavaScript
-          then "js"
-          else "html";
-      in ''
-        mkdir -p $out/share/doc
-        ${lib.concatStrings (map (module: ''
-            echo "compiling ${elmfile module}"
-            elm make ${elmfile module} --output $out/${module}.${extension} --docs $out/share/doc/${module}.json
-            ${lib.optionalString outputJavaScript ''
-              echo "minifying ${elmfile module}"
-              uglifyjs $out/${module}.${extension} --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
-                  | uglifyjs --mangle --output $out/${module}.min.${extension}
-            ''}
-          '')
-          targets)}
-      '';
-    };
 in
-  mkDerivation {
-    name = "elm-app-0.1.0";
+  stdenv.mkDerivation {
     src = ./.;
-    targets = ["Main"];
-    srcdir = "./src";
-    outputJavaScript = false;
+    name = "puzzle-draw-web";
+
+    buildInputs = [elmPackages.elm nodePackages.uglify-js];
+
+    buildPhase = pkgs.elmPackages.fetchElmDeps {
+      elmPackages = srcs;
+      elmVersion = "0.19.1";
+      registryDat = "./registry.dat";
+    };
+
+    installPhase = ''
+      elm make src/Main.elm --output $out/web.js
+      uglifyjs $out/web.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+                | uglifyjs --mangle --output $out/web.min.js
+    '';
   }
